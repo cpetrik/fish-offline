6d1d7a19c4469a9fbd37f700573fd8ed557fa4bf
Colleen Petrik <colleenpetrik@gmail.com>
2021-11-03 15:04:46 -0700

    Spinup expers w/1st 5 yrs

--- MatFEISTY/Make.m
+++ MatFEISTY/Make.m
@@ -4,25 +4,28 @@ clear all
 close all
 
 %%%%!! EXPERIMENTS
-spinup_cesm = false;
-fosi_cesm = true;
+spinup_cesm = true;
+fosi_cesm = false;
 
 tic
 
 if spinup_cesm
-%    Locs_CESM_4p4z_spinup() 
-    Spinup_cesm()
+%     Locs_CESM_4p4z_spinup()
+%     Spinup_cesm()
+    Spinup_FOSI_climatol()
+    Spinup_FOSI_varFood()
+    Spinup_FOSI_varTemp()
 %     Spinup_4p4z()
 %     Spinup_4p4z_comb()
 end
 if fosi_cesm
 %     Locs_CESM_4p4z()
-%    FOSI_cesm()
-%    FOSI_cesm_catch()
-   FOSI_cesm_climatol()
-   FOSI_cesm_varFood()
-   FOSI_cesm_varTemp()
-%    FOSI_cesm_search()
+%     FOSI_cesm()
+%     FOSI_cesm_catch()
+    FOSI_cesm_climatol()
+    FOSI_cesm_varFood()
+    FOSI_cesm_varTemp()
+%     FOSI_cesm_search()
 %     CESM_4p4z()
 %     CESM_4p4z_comb()
 end
