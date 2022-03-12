59d9244b6fc163fcf110a44fcbf31952d9fb9d49
Colleen Petrik <colleenpetrik@gmail.com>
2022-03-11 16:12:17 -0800

    Jack's MLD updates

--- MatFEISTY/cesm_mfiles/read_netcdf_jack_cce.m
+++ MatFEISTY/cesm_mfiles/read_netcdf_jack_cce.m
@@ -6,13 +6,10 @@ close all
 fpath='/Users/cpetrik/Dropbox/TAMU/iHESP/NSF_convergence/modeling/Jack/';
 
 %% All inputs
-ncdisp([fpath 'cclme_mld_40.nc'])
+ncdisp([fpath 'CCLME_HMXL_FINAL.nc'])
 
 %%
-
-
-%%
-ncid = netcdf.open([fpath 'cclme_mld_40.nc'],'NC_NOWRITE');
+ncid = netcdf.open([fpath 'CCLME_HMXL_FINAL.nc'],'NC_NOWRITE');
 
 [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
 
@@ -25,10 +22,19 @@ end
 netcdf.close(ncid);
 
 HMXL(HMXL>1e30) = nan;
+HMXL =double(HMXL);
 
 %%
-[lat,lon] = meshgrid(TLAT,TLONG);
-test = squeeze(HMXL(:,:,1));
+test = squeeze(HMXL(:,:,1,1));
+
+[ni,nj,nt,nm] = size(HMXL);
+t2=reshape(HMXL,ni*nj,nt,nm);
+mts = squeeze(nanmean(t2,1));
+
+%%
+[lat,lon] = meshgrid(TLAT,TLONG); 
+%TLAT:18.9-46.9; TLONG:223.8-251.9 = off
+%TLAT:21.9-48.4; TLONG:227.3-252.8 = should be
 
 plotminlat=18; %Set these bounds for your data
 plotmaxlat=50;
@@ -44,6 +50,22 @@ figure(1)
 axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
     'Grid','off','FLineWidth',1)
 surfm(lat,lon,test)
+% pcolor(lat,lon,test)
+% shading flat
+h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
+colorbar
+%caxis([0 1.1]);
+set(gcf,'renderer','painters')
+
+
+figure(2)
+plot(1:122,mts)
+
+%%
+figure(3)
+axesm ('Robinson','MapLatLimit',latlim,'MapLonLimit',lonlim,'frame','on',...
+    'Grid','off','FLineWidth',1)
+surfm(lat2,lon2,AllF(:,:,1))
 % pcolor(lat,lon,test)
 % shading flat
 h=patchm(coastlat+0.5,coastlon+0.5,'w','FaceColor',[0.75 0.75 0.75]);
